ff6ad68e84021a3ff083ed95dced11ed152f4aa9
Rubiksron <rubiksron@gmail.com>
2017-07-23 00:38:28 -0700

    I changed the position of the total column to the end of the table by moving the code that declared/created it to the end of the render function.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00noon', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm,', '9:00pm' ];
 
@@ -16,15 +16,15 @@ function CoffeeShop (minCust, maxCust, avgCups, locName) {
   console.log(this);
 }
 CoffeeShop.prototype.randomCustomer = function(min, max) {
-  return Math.floor(Math.random()*((max - min) + 1) + min);
-}
+  return Math.floor(Math.random() * ((max - min) + 1) + min);
+};
 CoffeeShop.prototype.generateHourly = function() {
   for (var i = 0; i < hours.length; i++) {
     var coffee = Math.ceil(this.avgCups * this.randomCustomer(this.minCust, this.maxCust));
     this.perHourArray.push(coffee);
     this.total += coffee;
   }
-}
+};
 CoffeeShop.prototype.renderShopRow = function() {
   this.generateHourly();
   var tableDataEl = document.getElementById('tableData');
@@ -33,18 +33,19 @@ CoffeeShop.prototype.renderShopRow = function() {
   var tdEl = document.createElement('td');
   tdEl.textContent = this.locName;
   trEl.appendChild(tdEl);
-  var tdElem = document.createElement('td');
-  tdElem.textContent = this.total;
-  trEl.appendChild(tdElem);
 
   for (var i = 0; i < this.perHourArray.length; i++) {
 
-    var tdEl = document.createElement('td');
+    tdEl = document.createElement('td');
     tdEl.textContent = this.perHourArray[i];
     trEl.appendChild(tdEl);
   }
+
+  var tdElem = document.createElement('td');
+  tdElem.textContent = this.total;
+  trEl.appendChild(tdElem);
   tableDataEl.appendChild(trEl);
-}
+};
 
 function renderHeaderRow() {
   var tableDataEl = document.getElementById('tableData');
@@ -53,16 +54,17 @@ function renderHeaderRow() {
   var thEl = document.createElement('th');
   thEl.textContent = 'location';
   trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = hours[i];
+    trEl.appendChild(tdEl);
+  }
+
   var thElem = document.createElement('th');
   thElem.textContent = 'total';
   trEl.appendChild(thElem);
-
-for (var i = 0; i < hours.length; i++) {
-  var tdEl = document.createElement('td');
-  tdEl.textContent = hours[i];
-  trEl.appendChild(tdEl);
-}
-tableDataEl.appendChild(trEl);
+  tableDataEl.appendChild(trEl);
 }
 renderHeaderRow();
 
