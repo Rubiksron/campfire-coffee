5806f92bead238630511b5833e3d846a06c3d2d0
Rubiksron <rubiksron@gmail.com>
2017-07-23 00:30:03 -0700

    cleaning up an older version of the salmon cookie project to reinforce the process.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00noon', '1:00pm', '2:00pm', '3:00pm', '4:00pm'];
+'use strict';
 
-var stores = ['pikePlaceMarket', 'capitolHill', 'seattlePublicLibrary', 'southLakeUnion', 'seaTacAirport', 'websiteSales'];
+var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00noon', '1:00pm', '2:00pm', '3:00pm', '4:00pm'];
+var stores = [];
+var tableDataEl = document.getElementById('tableData');
 
 function CoffeeShop (minCust, maxCust, avgCups, locName) {
   this.minCust = minCust;
@@ -11,54 +13,56 @@ function CoffeeShop (minCust, maxCust, avgCups, locName) {
   this.total = 0;
   this.renderShopRow();
 }
+
 CoffeeShop.prototype.randomCustomer = function(min, max) {
-  return Math.floor(Math.random()*((max - min) + 1) + min);
-}
+  return Math.floor(Math.random() * ((max - min) + 1) + min);
+};
+
 CoffeeShop.prototype.generateHourly = function() {
   for (var i = 0; i < hours.length; i++) {
     var coffee = Math.ceil(this.avgCups * this.randomCustomer(this.minCust, this.maxCust));
     this.perHourArray.push(coffee);
     this.total += coffee;
   }
-}
+  console.log(this.perHourArray, 'this.perHourArray');
+};
+
 CoffeeShop.prototype.renderShopRow = function() {
   this.generateHourly();
-  var tableDataEl = document.getElementById('tableData');
 
   var trEl = document.createElement('tr');
   var tdEl = document.createElement('td');
   tdEl.textContent = this.locName;
   trEl.appendChild(tdEl);
-  var tdElem = document.createElement('td');
-  tdElem.textContent = this.total;
-  trEl.appendChild(tdElem);
 
   for (var i = 0; i < this.perHourArray.length; i++) {
-
-    var tdEl = document.createElement('td');
+    tdEl = document.createElement('td');
     tdEl.textContent = this.perHourArray[i];
     trEl.appendChild(tdEl);
   }
+
+  var tdElem = document.createElement('td');
+  tdElem.textContent = this.total;
+  trEl.appendChild(tdElem);
   tableDataEl.appendChild(trEl);
-}
+};
 
 function renderHeaderRow() {
-  var tableDataEl = document.getElementById('tableData');
-
   var trEl = document.createElement('tr');
   var thEl = document.createElement('th');
   thEl.textContent = 'location';
   trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    var tdEl = document.createElement('td');
+    tdEl.textContent = hours[i];
+    trEl.appendChild(tdEl);
+  }
+
   var thElem = document.createElement('th');
   thElem.textContent = 'total';
   trEl.appendChild(thElem);
-
-for (var i = 0; i < hours.length; i++) {
-  var tdEl = document.createElement('td');
-  tdEl.textContent = hours[i];
-  trEl.appendChild(tdEl);
-}
-tableDataEl.appendChild(trEl);
+  tableDataEl.appendChild(trEl);
 }
 renderHeaderRow();
 
